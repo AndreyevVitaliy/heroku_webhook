94aabe80e23533ca0a87dde0577e2c88d61213df
vitaliy <andv@kvarta.kz>
2022-04-11 10:23:07 +0200

    Возвращаем функционал отправки хоть каких-то сообщений

--- main.py
+++ main.py
@@ -1,53 +1,58 @@
+import pprint
+
 import telebot
 import telebot.types as types
 import os
 from flask import Flask, request
 import logging
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+from google.oauth2 import service_account
+from googleapiclient.http import MediaIoBaseDownload,MediaFileUpload
+from googleapiclient.discovery import build
 
 token = '5178608519:AAGqDLaQzbTEuyaGC5R9-LoZ45Y_8DxQ3Qg'
 bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands='start')
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет ' + message.from_user.first_name)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Начать работу")
-    markup.add(item1)
+# @bot.message_handler(commands='start')
+# def start_message(message):
+#     bot.send_message(message.chat.id, 'Привет ' + message.from_user.first_name)
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     item1 = types.KeyboardButton("Начать работу")
+#     markup.add(item1)
 
-@bot.message_handler(commands=['button'])
-def button_message(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Кнопка")
-    markup.add(item1)
-    bot.send_message(message.chat.id,'' , reply_markup=markup)
+# @bot.message_handler(commands=['button'])
+# def button_message(message):
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     item1 = types.KeyboardButton("Кнопка")
+#     markup.add(item1)
+#     bot.send_message(message.chat.id,'' , reply_markup=markup)
+#
+#
+# @bot.chosen_inline_handler(func=lambda chosen_inline_result: True)
+# def test_chosen(chosen_inline_result):
+#     pass
+#     # Process all chosen_inline_result.
 
-
-@bot.chosen_inline_handler(func=lambda chosen_inline_result: True)
-def test_chosen(chosen_inline_result):
-    pass
-    # Process all chosen_inline_result.
-
-@bot.inline_handler(lambda query: query.query == 'text')
-def query_text(inline_query):
-    try:
-        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
-        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
-        bot.answer_inline_query(inline_query.id, [r, r2])
-    except Exception as e:
-        print(e)
+# @bot.inline_handler(lambda query: query.query == 'text')
+# def query_text(inline_query):
+#     try:
+#         r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+#         r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+#         bot.answer_inline_query(inline_query.id, [r, r2])
+#     except Exception as e:
+#         print(e)
 
 
 @bot.message_handler(content_types='text')
 def text_message(message):
-    pass
+    bot.send_message(message.chat_id, "Да")
 
 
 @bot.message_handler(content_types='photo')
 def download_photo(message):
-    bot.send_message(message.chat.id, 'Принято')
-
-
-
+   bot.send_message(message.chat_id, "Пока не работает функция")
 
 
 
